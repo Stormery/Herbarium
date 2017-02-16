59a9fb3bf080789b11cc9f7413ae058a32f7114f
Ayo-PC\Ayo <panstormery@gmail.com>
2017-02-16 20:30:02 +0000

    init

---
+++ desktop/src/com/stormery/herbarium/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.stormery.herbarium.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.stormery.herbarium.Herbarium;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Herbarium(), config);
+	}
+}
