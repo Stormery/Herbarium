f4a52a49f9d2a922e93fbe86e45f30a8816428e0
Ayo-PC\Ayo <panstormery@gmail.com>
2017-03-03 17:30:31 +0000

    SplashScreen refactor

--- core/src/com/stormery/herbarium/screens/SplashScreen.java
+++ core/src/com/stormery/herbarium/screens/SplashScreen.java
@@ -1,11 +1,5 @@
 package com.stormery.herbarium.screens;
 
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
-import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
-import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
-import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.stormery.herbarium.Herbarium;
 import com.stormery.herbarium.ui.SplashScreenButton;
 
@@ -13,8 +7,6 @@ import com.stormery.herbarium.ui.SplashScreenButton;
 //ekran powitalny
 public class SplashScreen extends AbstractScreen{
 
-	private Texture logoImg;
-	private ImageButton continueButton;
 	private SplashScreenButton splashScreenButton;
 	
 	public SplashScreen(Herbarium herbarium) {
@@ -26,16 +18,9 @@ public class SplashScreen extends AbstractScreen{
 	protected void init() {
 		
 		
-		initSplashScreen();
 		initContinueButton(herbarium);
 	}
 	
-	private void initSplashScreen() {
-		//TODO implement better assets loading when game grows
-		// change logoImg to my own one
-		logoImg = new Texture("Logo.png");
-		
-	}
 
 	private void initContinueButton(Herbarium herbarium){
 	//TODO get better button texture
